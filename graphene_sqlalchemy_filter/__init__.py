cf59ccbce2e7be664d737109eade09ecabd664f4
art1415926535 <art1415926535@yandex.ru>
2020-09-04 19:18:32 +0300

    release 1.12.0a

--- graphene_sqlalchemy_filter/__init__.py
+++ graphene_sqlalchemy_filter/__init__.py
@@ -3,5 +3,5 @@ from .connection_field import FilterableConnectionField
 from .filters import FilterSet
 
 
-__version__ = '1.11.2'
+__version__ = '1.12.0a'
 __all__ = ['__version__', 'FilterSet', 'FilterableConnectionField']

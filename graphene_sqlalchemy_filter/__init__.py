526d783d5e7f71e78bd8bb2557b8f562be82bb52
Gustavo Lima <gustavo.alvares.lima@gmail.com>
2023-09-24 16:52:22 +0100

    - Updated version and setup.py

--- graphene_sqlalchemy_filter/__init__.py
+++ graphene_sqlalchemy_filter/__init__.py
@@ -3,5 +3,5 @@ from .connection_field import FilterableConnectionField
 from .filters import FilterSet
 
 
-__version__ = '1.13.0'
-__all__ = ['__version__', 'FilterSet', 'FilterableConnectionField']
+__version__ = "1.14.0"
+__all__ = ["__version__", "FilterSet", "FilterableConnectionField"]
--- setup.py
+++ setup.py
@@ -4,44 +4,45 @@ import re
 from setuptools import find_packages, setup
 
 
-_version_re = re.compile(r'__version__\s+=\s+(.*)')
+_version_re = re.compile(r"__version__\s+=\s+(.*)")
 
 
-with open('graphene_sqlalchemy_filter/__init__.py', 'rb') as f:
-    version = str(
-        ast.literal_eval(_version_re.search(f.read().decode('utf-8')).group(1))
-    )
+with open("graphene_sqlalchemy_filter/__init__.py", "rb") as f:
+    version = str(ast.literal_eval(_version_re.search(f.read().decode("utf-8")).group(1)))
 
 
-with open('README.rst', encoding='utf-8') as f:
+with open("README.rst", encoding="utf-8") as f:
     long_description = f.read()
 
 
 requirements = [
-    'graphene-sqlalchemy>=2.1.0,<3',
-    'SQLAlchemy<2',
+    "graphene-sqlalchemy>=2.1.0",
+    "SQLAlchemy<2",
 ]
 
 
 setup(
-    name='graphene-sqlalchemy-filter',
+    name="graphene-sqlalchemy-filter",
     version=version,
-    description='Filters for Graphene SQLAlchemy integration',
-    url='https://github.com/art1415926535/graphene-sqlalchemy-filter',
-    author='Artem Fedotov',
-    license='MIT',
+    description="Filters for Graphene SQLAlchemy integration",
+    url="https://github.com/art1415926535/graphene-sqlalchemy-filter",
+    author="Artem Fedotov",
+    license="MIT",
     long_description=long_description,
     classifiers=[
-        'Intended Audience :: Developers',
-        'Topic :: Software Development :: Libraries',
-        'Programming Language :: Python :: 3',
-        'Programming Language :: Python :: 3.6',
-        'Programming Language :: Python :: 3.7',
-        'Programming Language :: Python :: 3.8',
-        'Programming Language :: Python :: Implementation :: PyPy',
+        "Intended Audience :: Developers",
+        "Topic :: Software Development :: Libraries",
+        "Programming Language :: Python :: 3",
+        "Programming Language :: Python :: 3.6",
+        "Programming Language :: Python :: 3.7",
+        "Programming Language :: Python :: 3.8",
+        "Programming Language :: Python :: 3.9",
+        "Programming Language :: Python :: 3.10",
+        "Programming Language :: Python :: 3.11",
+        "Programming Language :: Python :: Implementation :: PyPy",
     ],
-    keywords='api graphql protocol rest relay graphene',
-    packages=find_packages(exclude=['tests']),
+    keywords="api graphql protocol rest relay graphene",
+    packages=find_packages(exclude=["tests"]),
     install_requires=requirements,
-    python_requires='>=3.6'
+    python_requires=">=3.6",
 )
